1cdb8c4af8b0edbf3c6204e2db9d2f3d9ce11347
“samdesme” <samdesme@student.arteveldehs.be>
2018-10-10 20:21:04 +0200

    finished setting up events, edit readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # labo-3-ifttt-doors-samdesme
+
+## Opdracht
+Maak een applicatie waarmee we notificaties sturen naar de IFTTT-app (Koppelingen naar een externe site.)Koppelingen naar een externe site. (smartphone) wanneer de voordeur opengaat (Joystick UP) of sluit (Joystick Down), de achterdeur opengaat (Joystick LEFT) of sluit (Joystick RIGHT) en tenslotte de gegevens van de sensoren doorstuurt (drukknop Joystick). 
--- pi/app_ifttt_doors.py
+++ pi/app_ifttt_doors.py
@@ -6,22 +6,22 @@ sense = SenseHat()
 
 def joystickUp(event):
     if event.action == ACTION_RELEASED:
-        r = requests.post("https://maker.ifttt.com/trigger/front_door_opened/with/key/gT3jmnxkbXltZXqq7UhWFERRtyyZ6Ddru90SFtB1rya?value=frontdooropen")
+        r = requests.post("https://maker.ifttt.com/trigger/front_door_opened/with/key/gT3jmnxkbXltZXqq7UhWFERRtyyZ6Ddru90SFtB1rya?")
     
 def joystickDown(event):
     
     if event.action == ACTION_RELEASED:
-        r = requests.post("https://maker.ifttt.com/trigger/front_door_closed/with/key/gT3jmnxkbXltZXqq7UhWFERRtyyZ6Ddru90SFtB1rya?value=")
+        r = requests.post("https://maker.ifttt.com/trigger/front_door_closed/with/key/gT3jmnxkbXltZXqq7UhWFERRtyyZ6Ddru90SFtB1rya?")
 
 def joystickLeft(event):
     
     if event.action == ACTION_RELEASED:
-        r = requests.post("https://maker.ifttt.com/trigger/back_door_opened/with/key/gT3jmnxkbXltZXqq7UhWFERRtyyZ6Ddru90SFtB1rya?value=")
+        r = requests.post("https://maker.ifttt.com/trigger/back_door_opened/with/key/gT3jmnxkbXltZXqq7UhWFERRtyyZ6Ddru90SFtB1rya?")
 
 def joystickRight(event):
     
     if event.action == ACTION_RELEASED:
-        r = requests.post("https://maker.ifttt.com/trigger/back_door_closed/with/key/gT3jmnxkbXltZXqq7UhWFERRtyyZ6Ddru90SFtB1rya?value=")
+        r = requests.post("https://maker.ifttt.com/trigger/back_door_closed/with/key/gT3jmnxkbXltZXqq7UhWFERRtyyZ6Ddru90SFtB1rya?")
 
 sense.stick.direction_up = joystickUp
 sense.stick.direction_down = joystickDown
