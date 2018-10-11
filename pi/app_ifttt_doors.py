f31f577457394131581661f10056093192daaf45
“samdesme” <samdesme@student.arteveldehs.be>
2018-10-11 18:24:39 +0200

    final push: sensor value

--- pi/app_ifttt_doors.py
+++ pi/app_ifttt_doors.py
@@ -23,8 +23,26 @@ def joystickRight(event):
     if event.action == ACTION_RELEASED:
         r = requests.post("https://maker.ifttt.com/trigger/back_door_closed/with/key/gT3jmnxkbXltZXqq7UhWFERRtyyZ6Ddru90SFtB1rya?")
 
+def joystickMiddle(event):
+    if event.action == ACTION_RELEASED:
+
+     temp = "{}°C".format(sense.get_temperature())
+     hum = "{}%".format(sense.get_humidity())
+     press = "{}.Ombar".format(sense.get_pressure())
+
+     r = requests.post("https://maker.ifttt.com/trigger/sensors/with/key/gT3jmnxkbXltZXqq7UhWFERRtyyZ6Ddru90SFtB1rya?",
+                     data = {
+                       "value1" : temp,
+                       "value2" : hum,
+                       "value3" : press          
+                           })
+
+     print(temp, hum, press)
+
 sense.stick.direction_up = joystickUp
 sense.stick.direction_down = joystickDown
 sense.stick.direction_left = joystickLeft
 sense.stick.direction_right = joystickRight
+sense.stick.direction_middle = joystickMiddle
+
 pause()
